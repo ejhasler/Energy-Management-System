b1fbbabb242bd17aaf871069e26d46ab3f198958
Harald <haraldwf@stud.ntnu.no>
2022-10-27 12:12:06 +0200

    Date and time
    Added date and time variable in the Analyze class for later usage.

--- src/main/java/no/ntnu/IDATA2304_Networks/Group10/backEnd/Logic/Analyze.java
+++ src/main/java/no/ntnu/IDATA2304_Networks/Group10/backEnd/Logic/Analyze.java
@@ -1,5 +1,7 @@
 package no.ntnu.IDATA2304_Networks.Group10.backEnd.Logic;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,6 +21,8 @@ public class Analyze {
     private int smallValue;     // Represent small value in energy usage
     private int usage;          // Represent the usage of the energy meter specific for that room
     private int totalUsage;     // Represent the total usage of a day/week/month/year
+    private LocalDate date;     // Represent the date of when the energy usage took place
+    private LocalTime time;     // Represent the time of day when the energy usage took place
 
 
     private List<ReceiverRasp> dataList;
