64899b7f2d5e2d33479ce0146cec68064a7b315a
Harald <haraldwf@stud.ntnu.no>
2022-11-09 12:22:42 +0100

    Updated App class
    Updated app class with methods to initialize sensors

--- src/main/java/no/ntnu/IDATA2304_Networks/Group10/backEnd/App.java
+++ src/main/java/no/ntnu/IDATA2304_Networks/Group10/backEnd/App.java
@@ -1,27 +1,59 @@
 package no.ntnu.IDATA2304_Networks.Group10.backEnd;
 
 public class App {
+
     private static final long SLEEP_DURATION_MS = 4000;
+    double lastKwReading;
 
+    /**
+     * Run the application
+     * @throws InterruptedException
+     */
     public void run() throws InterruptedException{
-
+        initializeSensors();
         while(true) {
             receiveDataFromSensor();
-            sendDataForward();
+            sendDataToServer();
             sensorPauseBetweenReading();
         }
     }
 
+    /**
+     * Initializes all the sensors
+     *
+     * @throws IllegalStateException if some sensors are not found
+     */
+    private void initializeSensors() {
+//        SensorProvider sensorProvider = SensorProvider.getInstance();
+//        kwSensor = sensorProvider.getKwSensor();
+//        if (kwSensor == null) {
+//            throw new IllegalStateException("Temperature sensor not found");
+//        }
+    }
+
     private void receiveDataFromSensor() {
         System.out.println("Recieving data from sensor....");
+        lastKwReading = readKwSensor();
     }
 
-    private void sendDataForward(){
-        System.out.println("Forwarding the packets...");
+    private double readKwSensor() {
+//        return lastKwReading.readValue();
+        //
+        return 20;
     }
 
-    private void sensorPauseBetweenReading() throws InterruptedException {
-        System.out.println("Waiting for pause to end!");
-        Thread.sleep(SLEEP_DURATION_MS);
+
+    private void sendDataToServer(){
+        System.out.println("Sending data to server:");
+        System.out.println("    kw: " + lastKwReading + "Kw");
+        System.out.println("");
+    }
+
+    private void sensorPauseBetweenReading() {
+        try {
+            Thread.sleep(SLEEP_DURATION_MS);
+        } catch (InterruptedException e) {
+            System.out.println("ERROR: Pause was interrupted");
+        }
     }
 }
