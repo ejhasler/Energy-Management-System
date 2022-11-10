80aa65d40c36f26f51c1ae4d45d1964a6c3f237b
Jakob Finstad <jakobf44@gmail.com>
2022-11-10 12:17:12 +0100

    Changing the pom file for adapting to javafx application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module IoT.Project.Group10 {
+  requires javafx.controls;
+
+  exports no.ntnu.IDATA2304_Networks.Group10.frontEnd;
+}
---
+++ src/main/java/no/ntnu/IDATA2304_Networks/Group10/frontEnd/App.java
@@ -0,0 +1,4 @@
+package no.ntnu.IDATA2304_Networks.Group10.frontEnd;
+
+public class App {
+}
