adfc7683e165b694597adcc5bca664e3f1c95f78
JakobFinstad <jakobf44@gmail.com>
2022-11-14 13:35:02 +0100

    Setting up the main front end

---
+++ src/main/java/no/ntnu/IDATA2304_Networks/Group10/Main.java
@@ -0,0 +1,7 @@
+package no.ntnu.IDATA2304_Networks.Group10;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/no/ntnu/IDATA2304_Networks/Group10/frontEnd/App.java
+++ src/main/java/no/ntnu/IDATA2304_Networks/Group10/frontEnd/App.java
@@ -1,20 +1,4 @@
 package no.ntnu.IDATA2304_Networks.Group10.frontEnd;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import no.ntnu.IDATA2304_Networks.Group10.frontEnd.MainWindow;
-
-import static javafx.application.Application.launch;
-
-public class App extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        MainWindow mainWindow = new MainWindow();
-    }
-
+public class App {
 }
